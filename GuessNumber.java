e1f8f147c388f0779b6a0899fc3b18ec85825f26
L-win <l-win@inbox.ru>
2019-09-03 21:07:57 +0300

    added: base game algorithm

--- GuessNumber.java
+++ GuessNumber.java
@@ -3,21 +3,25 @@ import java.util.Random;
 
 class GuessNumber{
   private Scanner input;
-  private int number;
+  private static int number;
   private int userInput;
   public static void main(String a[]){
     // Start game
     GuessNumber game = new GuessNumber();
-    game.setNumber();
-    game.readUserInput();
-    game.round();
+    setNumber();
+    game.start();
   };
-  private void setNumber(){
+  private void start(){
+    //setNumber();
+    readUserInput();
+    round();
+  };
+  private static void setNumber(){
     // Set random number
     Random random = new Random();
     number = random.nextInt(100);
     number += 1;
-    //number = 1; 
+    //number = 5;
   };
   private void readUserInput(){
     // Read user input
@@ -27,6 +31,20 @@ class GuessNumber{
     userInput = input.nextInt();
   };
   private void round(){
-    
+    if (userInput > number ){
+      System.out.println("\nYour guess is GREATER..");
+      GuessNumber game = new GuessNumber();
+      game.start();
+      //System.out.println(""+game.number);
+    }
+    else if(userInput < number){
+      System.out.println("\nYour guess is LOWER..");
+      GuessNumber game = new GuessNumber();
+      game.start();
+      //System.out.println(""+game.number);
+    }
+    else{
+      System.out.println("\nCorrect!\n");
+    }
   };
 }
