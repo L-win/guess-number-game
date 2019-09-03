7936f833b4bb5b288f6629abf8597f5d18dc0460
L-win <l-win@inbox.ru>
2019-09-03 19:51:28 +0300

    Initial commit

---
+++ GuessNumber.java
@@ -0,0 +1,12 @@
+class GuessNumber{
+  int number;
+  static main(String a[]){};
+  void setNumber(){
+    // Set random number
+  };
+  void readUserInput(){
+    // Read user input
+    // Check if it is int
+  };
+  void round(){};
+}
