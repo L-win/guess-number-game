b5af39a22d7f5b489d6877851cbd837d7415a625
L-win <l-win@inbox.ru>
2019-09-03 20:42:51 +0300

    added: set random number added: get user input

--- GuessNumber.java
+++ GuessNumber.java
@@ -1,12 +1,32 @@
+import java.util.Scanner;
+import java.util.Random;
+
 class GuessNumber{
-  int number;
-  static main(String a[]){};
-  void setNumber(){
-    // Set random number
+  private Scanner input;
+  private int number;
+  private int userInput;
+  public static void main(String a[]){
+    // Start game
+    GuessNumber game = new GuessNumber();
+    game.setNumber();
+    game.readUserInput();
+    game.round();
   };
-  void readUserInput(){
+  private void setNumber(){
+    // Set random number
+    Random random = new Random();
+    number = random.nextInt(100);
+    number += 1;
+    //number = 1; 
+  };
+  private void readUserInput(){
     // Read user input
     // Check if it is int
+    input = new Scanner(System.in);
+    System.out.println("\nEnter number:");
+    userInput = input.nextInt();
   };
-  void round(){};
+  private void round(){
+    
+  };
 }
