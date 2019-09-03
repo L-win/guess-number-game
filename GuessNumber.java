5d8122b0ab8e899538d9a45450bf141820f48627
L-win <l-win@inbox.ru>
2019-09-03 21:18:52 +0300

    edited: commented lines removed added: readme file

--- GuessNumber.java
+++ GuessNumber.java
@@ -6,28 +6,23 @@ class GuessNumber{
   private static int number;
   private int userInput;
   public static void main(String a[]){
-    // Start game
     GuessNumber game = new GuessNumber();
+    System.out.println("\nGuess the number between 1 and 100.");
     setNumber();
     game.start();
   };
   private void start(){
-    //setNumber();
     readUserInput();
     round();
   };
   private static void setNumber(){
-    // Set random number
     Random random = new Random();
     number = random.nextInt(100);
     number += 1;
-    //number = 5;
   };
   private void readUserInput(){
-    // Read user input
-    // Check if it is int
     input = new Scanner(System.in);
-    System.out.println("\nEnter number:");
+    System.out.println("Enter number:");
     userInput = input.nextInt();
   };
   private void round(){
@@ -35,13 +30,11 @@ class GuessNumber{
       System.out.println("\nYour guess is GREATER..");
       GuessNumber game = new GuessNumber();
       game.start();
-      //System.out.println(""+game.number);
     }
     else if(userInput < number){
       System.out.println("\nYour guess is LOWER..");
       GuessNumber game = new GuessNumber();
       game.start();
-      //System.out.println(""+game.number);
     }
     else{
       System.out.println("\nCorrect!\n");
---
+++ README.md
@@ -0,0 +1,11 @@
+## About
+Guess the number game.
+
+## How to use
+```
+java GuessNumber
+```
+
+## Todo
+* check entered value type
+* user sets max value
